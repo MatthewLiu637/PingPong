accc0740a7930e619623026a792dce972ac1a97b
MatthewLiu637 <78493600+MatthewLiu637@users.noreply.github.com>
2022-09-01 18:34:41 -0400

    Added Ping Pong File, pictures for racket and tennis ball
    Added Functionality for the Rackets.
    
    Pictures added as well

---
+++ PingPong.py
@@ -0,0 +1,74 @@
+from pygame import *
+import os.path
+
+#game scene
+background_color = (0, 128, 0)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(background_color)
+
+#flags responsible for game state
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, x, y, speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#Subclass
+class Player(GameSprite):
+    def RIGHT_UPDATE(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
+    def LEFT_UPDATE(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
+
+filepath = os.path.dirname(__file__)
+r = os.path.join(filepath, "racket.png")
+t = os.path.join(filepath, "tennis_ball.png")
+
+racket1 = Player(r, 30, 200, 4, 50, 150) #image, x, y, speed, width, height
+racket2 = Player(r, 520, 200, 4, 50, 150) #image, x, y, speed, width, height
+ball = GameSprite(t, 200, 200, 4, 50, 50) #image, x, y, speed, width, height
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE', True, (180, 180, 0))
+lose2 = font.render('PLAYER 2 LOSE', True, (180, 180, 0))
+
+x = 3
+y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.fill(background_color)
+        racket1.LEFT_UPDATE()
+        racket2.RIGHT_UPDATE()
+
+        racket1.reset()
+        racket2.reset()
+    
+    display.update()
+    clock.tick(FPS)
